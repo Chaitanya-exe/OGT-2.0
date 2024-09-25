b4de87d684e72c5d1608b1347597df829e5d5c4f
Chaitany-exe <jeetkrishna3333@gmail.com>
2024-09-25 22:20:21 +0530

    merge conflict solved

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,10 @@
-<<<<<<< HEAD
 // import { Inter } from "next/font/google";
 import "@/style/globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-=======
 import { Inter } from "next/font/google";
 import Provider from "@/components/Provider"
 import "./globals.css";
->>>>>>> 43640c4393539b0396f27a003596995617b29e67
 
 // const inter = Inter({ subsets: ["latin"] });
 
@@ -19,18 +16,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-<<<<<<< HEAD
       <body className="bg-bgColor text-blac relative">
-      <Header/>
-      {children}
-      <Footer/>
-
-=======
-      <body className={inter.className}>
         <Provider>
+          <Header />
           {children}
+          <Footer />
         </Provider>
->>>>>>> 43640c4393539b0396f27a003596995617b29e67
       </body>
     </html>
   );
--- app/page.js
+++ app/page.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 import Tabs from "@/components/Tabs";
 import Video from "@/components/Video";
@@ -14,62 +13,6 @@ export default function Home() {
       <div>
         <Tabs direction="left" />
         <Tabs direction="right" />
-=======
-"use client";
-import Link from "next/link";
-import { useState, useEffect } from "react";
-import Image from "next/image";
-import { signIn, signOut, useSession, getProviders } from "next-auth/react";
-
-export default function Home() {
-  const { data: session } = useSession();
-  const [providers, setProviders] = useState(null);
-
-  useEffect(() => {
-    const setUpProviders = async () => {
-      const response = await getProviders();
-      setProviders(response);
-    }
-    setUpProviders();
-
-  }, [])
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <div className="sm:flex hidden">
-            {session?.user ? (
-              <div className="flex gap-3 md:gap-5">
-
-                <button type="button" className="outline_btn" onClick={() => signOut()}>
-                  sign out
-                </button>
-                <Link href="/">
-                  <Image
-                    src={session?.user.image}
-                    alt="profile"
-                    width={37}
-                    height={37}
-                    className="rounded-full"
-                  />
-                </Link>
-              </div>
-            ) : (<>{providers && Object.values(providers).map((provider) => {
-              return (
-                <button
-                  className="black_btn"
-                  type="button"
-                  key={provider.name}
-                  onClick={() => signIn(provider.id)}> Sign In</button>
-              )
-            })}</>)}
-          </div>
-        </div>
->>>>>>> 43640c4393539b0396f27a003596995617b29e67
       </div>
       <Video />
       <Sphere/>
