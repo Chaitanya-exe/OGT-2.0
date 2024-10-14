2456d95b0c743b62eead91c4538c08dd1e85c20f
Chaitany-exe <jeetkrishna3333@gmail.com>
2024-10-14 09:35:43 +0530

    database migration

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -10,7 +10,7 @@ const handler = NextAuth({
         GoogleProvider({
             clientId: process.env.CLIENT_ID,
             clientSecret: process.env.CLIENT_SECRET,
-            
+
         })
     ],
     callbacks: {
@@ -25,7 +25,7 @@ const handler = NextAuth({
                 userClient.$disconnect();
                 return session
             } catch (error) {
-                writeFileSync(path.join(__dirname,"errors.log"),`${error}\n\n`);
+                writeFileSync(path.join(__dirname, "errors.log"), `${error}\n\n`);
                 console.log("There is an error");
                 return
             }
@@ -33,27 +33,30 @@ const handler = NextAuth({
 
         async signIn({ profile }) {
             try {
-                
-                const userClient = new PrismaClient();
-                const isUser = await userClient.users.findUnique({where:{
-                    email: profile.email
-                }});
-                if(!isUser){
+
+                const isUser = await userClient.users.findUnique({
+                    where: {
+                        email: profile.email
+                    }
+                });
+                console.log(isUser);
+                if (!isUser) {
                     const username = profile.email.split("@")[0];
-                    const response = await userClient.users.create({data:{
-                        email: profile.email,
-                        name: profile.name,
-                        Username: username,
-                        img: `${profile.picture}`,
-                        
-                    }})
+                    const response = await userClient.users.create({
+                        data: {
+                            email: profile.email,
+                            name: profile.name,
+                            Username: username,
+                            img: `${profile.picture}`,
+
+                        }
+                    })
                     console.log(response);
                 }
-                console.log(isUser);
                 userClient.$disconnect();
                 return true;
             } catch (error) {
-                writeFileSync(path.join(__dirname,"errors.log"), `${error}\n\n`);
+                writeFileSync(path.join(__dirname, "errors.log"), `${error}\n\n`);
                 console.log("There is an error");
                 return false
             }
@@ -61,4 +64,4 @@ const handler = NextAuth({
     }
 })
 
-export {handler as GET, handler as POST};
+export { handler as GET, handler as POST };
