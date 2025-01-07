1d350114887746c97ddb7462239949ebe85d0975
Chaitany-exe <jeetkrishna3333@gmail.com>
2025-01-07 16:24:49 +0530

    package update

--- app/api/get-blogs/[id]/route.js
+++ app/api/get-blogs/[id]/route.js
@@ -1,32 +1,32 @@
-import { fetchId,createError } from "@/utils";
+import { fetchId, createError } from "@/utils";
 import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
 
 const userClient = new PrismaClient();
 
-async function handler(req){
+async function handler(req) {
     try {
         const blogId = fetchId(req.url);
-        if (blogId){
+        if (blogId) {
             const blog = await userClient.blogs.findUnique({
                 where: {
                     id: blogId
                 }
             })
-            if(blog){
-                return Response.json({blog},{status:200});
+            if (blog) {
+                return Response.json({ blog }, { status: 200 });
             }
-            return NextResponse.json({error:"Blogs not found"},{status:404});
-        } else{
+            return NextResponse.json({ error: "Blogs not found" }, { status: 404 });
+        } else {
             throw new Error("Blog Id not present or unable to fetch");
         }
     } catch (error) {
         console.log(error);
         createError(error);
-        return Response.json({error: error.message},{status:500});
-    } finally{
+        return Response.json({ error: error.message }, { status: 500 });
+    } finally {
         await userClient.$disconnect();
     }
 }
 
-export {handler as GET}
+export { handler as GET }
